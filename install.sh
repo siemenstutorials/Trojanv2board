507c0c53eb7483c5304592145e6c263650ad4db2
Siemens Tutorials <55917384+siemenstutorials@users.noreply.github.com>
2022-08-24 00:13:46 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -257,14 +257,20 @@ install_XrayR() {
     echo "---------------------------"
     echo ""
 
+#Default
+
+link=https://vturay.com
+vkey=fab93f2c596
+dom_name=node1.test.com
+
 #设置Config.yml
     echo "正在尝试写入配置信息..."
     wget https://raw.githubusercontent.com/siemenstutorials/Trojanv2board/master/config.yml -O /etc/XrayR/config.yml
     sed -i "s/NodeID:.*/NodeID: ${node_id}/g" /etc/XrayR/config.yml
     sed -i "s/NodeType:.*/NodeType: ${node_type}/g" /etc/XrayR/config.yml
-    sed -i "s/ApiHost:.*/ApiHost: "${Api_url}"/g" /etc/XrayR/config.yml
-    sed -i "s/ApiKey:.*/ApiKey: "${Api_key}"/g" /etc/XrayR/config.yml
-    sed -i "s/CertDomain:.*/CertDomain: "${Node_domain}"/g" /etc/XrayR/config.yml
+    sed -i "s|$link|${Api_url}|" /etc/XrayR/config.yml
+    sed -i "s|$vkey|${Api_key}|" /etc/XrayR/config.yml
+    sed -i "s|${dom_name}|${Node_domain}|" /etc/XrayR/config.yml
     sed -i "s/CLOUDFLARE_EMAIL:.*/CLOUDFLARE_EMAIL: ${Cf_mail}/g" /etc/XrayR/config.yml
     sed -i "s/CLOUDFLARE_API_KEY:.*/CLOUDFLARE_API_KEY: ${Cf_key}/g" /etc/XrayR/config.yml
     echo ""
